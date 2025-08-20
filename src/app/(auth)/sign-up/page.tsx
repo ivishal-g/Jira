5669bdcaf37daaf70b79e17cd161791030bc82b4
Vishal Gaikwad <gaikwad8vishal@gmail.com>
2025-08-20 12:13:25 +0530

    02:sign-template

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { SignUpCard } from "@/features/auth/components/sign-up-card";
+
+
+const SignUpPage = () => {
+    return (
+        <SignUpCard/>
+        )
+}
+
+export default SignUpPage;
